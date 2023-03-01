18f1542f3ef27f7f067ffa0bf0f8bc8f4f7b87cf
HakaiCoding <hakaicoding@gmail.com>
2023-03-01 18:11:31 +0100

    01/03/2023

---
+++ DWEC/10_Juegos/01_Wordle/script.js
@@ -0,0 +1,11 @@
+// Array con 10 palabras
+const WORDS = ['Reloj', 'Pinza', 'Negro', 'Oveja', 'Casco', 'Plato', 'Tarta', 'Cinco', 'Cebra', 'Mosca'];
+
+const GAME_BOX = document.getElementById('game-box');
+const GAME_FIRST_ROW = document.getElementById('game-first-row');
+let firstRowFirstColumn
+const GAME_SECOND_ROW = document.getElementById('game-second-row');
+const GAME_THIRD_ROW = document.getElementById('game-third-row');
+const GAME_FOURTH_ROW = document.getElementById('game-fourth-row');
+const GAME_FIFTH_ROW = document.getElementById('game-fifth-row');
+const GAME_SIXTH_ROW = document.getElementById('game-sixth-row');
